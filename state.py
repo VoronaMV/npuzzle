f27d333c6ac6d4eea413221babd6b9232a3e9805
mvorona <mv@morbax.com>
2018-11-14 21:22:30 +0200

    work with simple heuristics. Other hueristics should be added. Speed should be faster

--- state.py
+++ state.py
@@ -1,3 +1,6 @@
+import os
+import re
+import sys
 import numpy as np
 from typing import Deque
 
@@ -12,22 +15,40 @@ class NPuzzlesMap:
         self.initial_map = initial_map
         self.initial_state = State(self.initial_map)
         dimension, _ = shape
-        terminal_array = np.append(np.arange(1, dimension**2), 0)
+        terminal_flat_array = np.append(np.arange(1, dimension**2), 0)
+        terminal_array = np.reshape(terminal_flat_array, shape)
         self.terminal_state = State(terminal_array)
         # State.terminal_state = self.terminal_state
+
+    @staticmethod
+    def __map_from_file(filename: str) -> np.ndarray:
+        dimension = None
+        start_map = []
+        if not os.path.isfile(filename):
+            raise Exception(f'File {filename} does not exist')
+        with open(filename, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if dimension is None and re.fullmatch(r'\d+', line):
+                    dimension = int(line)
+                elif dimension is not None:
+                    row = re.findall(r'\d+', line)
+                    row = [int(digit) for digit in row[:dimension]]
+                    start_map.append(row)
+        return np.array(start_map)
 
     @classmethod
     def from_file(cls, filename):
         # TODO: Implement file reading here
-        shape = (0, 0)
-        initial_map = np.ndarray()
-        cls(shape, initial_map)
+        initial_map = cls.__map_from_file(filename)
+        # shape = initial_map.shape
+        return cls(initial_map.shape, initial_map)
 
     def __str__(self):
         return f'Initial{self.initial_state}, Terminal{self.terminal_state}'
 
     def __repr__(self):
-        return  self.__str__()
+        return self.__str__()
 
     class BadMapError(Exception):
         def __init__(self, message='Bad map', error=None):
@@ -41,7 +62,7 @@ class State:
 
     def __init__(self, data: np.ndarray, parent=None, ):
         if not isinstance(data, np.ndarray) and data.size < 9:
-            raise state.BadMapError()
+            raise State.BadMapError()
         self._map = data.astype(int)
         self.flat_map = self._map.flatten()
         self.parent = parent
@@ -49,7 +70,10 @@ class State:
         # TODO: Make better way
         if self.terminal_map is None:
             dimension, _ = self._map.shape
-            self.terminal_map = np.append(np.arange(1, dimension ** 2), 0)
+            terminal_flat_array = np.append(np.arange(1, dimension ** 2), 0)
+            self.terminal_map = np.reshape(terminal_flat_array, self._map.shape)
+
+        self.empty_puzzle_coord = self.empty_element_coordinates(self._map)
 
     def __eq__(self, other) -> bool:
         if not isinstance(other, self.__class__):
@@ -57,7 +81,9 @@ class State:
         return np.array_equal(self._map, other._map)
 
     def __str__(self):
-        return f'State(f={self.h}, g={self.g}, h={self.h})'
+        # return f'State(f={self.h}, g={self.g}, h={self.h})'
+        # return f'State(g={self.g})'
+        return str(self._map)
 
     def __repr__(self):
         return self.__str__()
@@ -72,6 +98,27 @@ class State:
             super().__init__(message)
             self.error = error
 
+    def shift_empty_puzzle(self, direction):
+        row_indx, col_indx = self.empty_puzzle_coord
+        directions = {
+            'up': (row_indx - 1, col_indx),
+            'down': (row_indx + 1, col_indx),
+            'right': (row_indx, col_indx + 1),
+            'left': (row_indx, col_indx - 1)
+        }
+        try:
+            new_coords = directions[direction]
+        except KeyError:
+            raise Exception(f'Wrong Direction. Available are: {direction.keys()}')
+        else:
+            return new_coords
+
+    @staticmethod
+    def empty_element_coordinates(_map: np.ndarray) -> tuple:
+        for indx_pair, elem in np.ndenumerate(_map):
+            if elem == 0:
+                return indx_pair
+
     @property
     def all_neighbours(self):
         """
@@ -80,39 +127,35 @@ class State:
         """
         raise NotImplementedError()
 
-    @property
-    def h(self) -> int:
-        """
-        Returns a number of puzzles at wrong place.
-        """
-        if not isinstance(self._map, np.ndarray) and self._map.size < 9:
-            raise self.BadMapError()
-
-        wrong_placed_puzzles = 0
-        for i, puzzle in enumerate(self.flat_map):
-            if puzzle == 0 and i + 1 != len(self.flat_map):
-                continue
-            elif puzzle != i + 1:
-                wrong_placed_puzzles += 1
-
-        return wrong_placed_puzzles
+    # @property
+    # def h(self) -> int:
+    #     """
+    #     Returns a number of puzzles at wrong place.
+    #     """
+    #     if not isinstance(self._map, np.ndarray) and self._map.size < 9:
+    #         raise self.BadMapError()
+    #
+    #     wrong_placed_puzzles = 0
+    #     for i, puzzle in enumerate(self.flat_map):
+    #         if puzzle == 0 and i + 1 != len(self.flat_map):
+    #             continue
+    #         elif puzzle != i + 1:
+    #             wrong_placed_puzzles += 1
+    #
+    #     return wrong_placed_puzzles
 
     # @property
-    # def terminal_state(self):
-    #     np.append(np.arange(1, dimension ** 2), 0)
-    #     return
-
-    @property
-    def is_terminate(self) -> bool:
-        """
-        Check if all puzzles at its places
-        :return: bool
-        """
-        return self.h == 0
-
-    @property
-    def f(self) -> int:
-        return self.g + self.h
+    # def is_terminate(self) -> bool:
+    #     """
+    #     Check if all puzzles at its places
+    #     :return: bool
+    #     """
+    #     # return self._map == self.
+    #     return self.h == 0
+    #
+    # @property
+    # def f(self) -> int:
+    #     return self.g + self.h
 
 
 # TODO: Do we need Dequee?
@@ -134,12 +177,14 @@ class TState(Deque):
     def is_terminate(self):
         raise NotImplementedError()
 
-    @property
-    def min_state(self) -> State:
+    def find_min_state(self, heuristic: callable) -> State:
         min_state = self[0]
+        min_state_f = min_state.g + heuristic(min_state)
         for elem in self:
-            if elem.f < min_state.f:
+            elem_f = elem.g + heuristic(elem)
+            if elem_f < min_state_f:
                 min_state = elem
+                min_state_f = elem_f
         return min_state
 
     @staticmethod
@@ -151,6 +196,12 @@ class TState(Deque):
     def __contains__(self, item: State) -> bool:
         matches = (True for state in self if item == state)
         return next(matches, False)
+
+    def __str__(self):
+        res = ''
+        for elem in self:
+            res += str(elem) + '\n\n'
+        return res
 
 
 class Rule:
@@ -208,12 +259,27 @@ class Rule:
         return state.h == 0
 
     @staticmethod
-    def neignbours(elem: State) -> TState:
+    def neignbours(node: State) -> list:
         """
         Should return set of app states that can be neighbours to current
         map state (shift 0 right/left/up/down)
         """
-        raise NotImplementedError()
+        neighbours = list()
+        directions = ['up', 'down', 'right', 'left']
+        for direction in directions:
+            coordinates = node.shift_empty_puzzle(direction)
+            try:
+                if coordinates[0] < 0 and coordinates[1] < 0:
+                    raise Exception()
+                # TODO: switch elements
+                new_map = node._map.copy()
+                non_empty_element = node._map[coordinates]
+                new_map[node.empty_puzzle_coord] = non_empty_element
+                new_map[coordinates] = 0
+                neighbours.append(State(new_map))
+            except:
+                continue
+        return neighbours
 
     @staticmethod
     def h(state: State, model: str) -> int:
@@ -236,26 +302,24 @@ class Rule:
 
 
 if __name__ == '__main__':
-    arr = np.ones([3, 3])
-    state = State(arr)
-    print(state)
 
-    # TODO: Read from file, get map and create StartState
-    read_map = arr
+    heuristics_name = sys.argv[1].lower()
+    Rule.choose_heuristics(heuristics_name)
 
-    start_state = State(read_map)
+    # npazzle = NPuzzlesMap.from_file('4_4_map.txt')
+    npazzle = NPuzzlesMap.from_file('3_3_map_test.txt')
+
+    initial_state = npazzle.initial_state
+    terminal_state = npazzle.terminal_state
 
     _open = TState()
     _close = TState()
-    _open.append(start_state)
-
-    # next_state = State(read_map, parent=start_state)
-    # _open.append(next_state)
-    # print('min state: ', _open.min_state)
+    _open.append(initial_state)
 
     while _open:
-        min_state = _open.min_state
-        if Rule.is_terminate(min_state):
+        min_state = _open.find_min_state(Rule._heuristics)
+        if min_state == terminal_state: # check if current state is terminal
+        # if Rule.is_terminate(min_state):
             solution = TState(elem for elem in _open.reverse_to_head(min_state))
             solution.reverse()  # now it is solution
             exit(str(solution))
@@ -266,7 +330,7 @@ if __name__ == '__main__':
         for neighbour in neighbours:
             g = min_state.g + Rule.distance(min_state, neighbour)
 
-            if neighbours in _close:
+            if neighbour in _close:
                 continue
             is_g_better = False
 
