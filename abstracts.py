f0dbcd18524903fff985a16b5c1e4246b44a3345
sandruse <andrusechko@gmail.com>
2018-11-22 17:44:15 +0200

    decomposed project

---
+++ abstracts.py
@@ -0,0 +1,124 @@
+import numpy as np
+from hashlib import sha1
+from abc import ABCMeta, abstractmethod, abstractclassmethod, abstractstaticmethod
+from queue import PriorityQueue
+from typing import Deque
+from utils import TERMINAL_STATES
+
+
+class StateABC(metaclass=ABCMeta):
+
+    terminal_map = None
+
+    def __init__(self, data: np.ndarray, parent=None, heuristic: callable=None):
+        if not isinstance(data, np.ndarray) and data.size < 9:
+            raise StateABC.BadMapError()
+        self._map = data.astype(int)
+        self.flat_map = self._map.flatten()
+        self.parent = parent
+        self.g = parent.g + 1 if parent else 0
+        # TODO: Make better way
+        if self.terminal_map is None:
+            dimension, _ = self._map.shape
+            self.terminal_map = TERMINAL_STATES.get(dimension)
+
+        self.empty_puzzle_coord = self.empty_element_coordinates(self._map)
+        if heuristic:
+            self.f = g + heuristic(self)
+        else:
+            self.f = None
+        self.hash = sha1(self._map).hexdigest()
+
+    @staticmethod
+    def empty_element_coordinates(_map: np.ndarray) -> tuple:
+        for indx_pair, elem in np.ndenumerate(_map):
+            if elem == 0:
+                return indx_pair
+
+    @abstractmethod
+    def __eq__(self, other) -> bool:
+        pass
+
+    @abstractmethod
+    def __str__(self) -> str:
+        pass
+
+    @abstractmethod
+    def __repr__(self) -> str:
+        pass
+
+    @abstractmethod
+    def __lt__(self, other) -> bool:
+        pass
+
+    @abstractmethod
+    def __le__(self, other) -> bool:
+        pass
+
+    @abstractmethod
+    def __gt__(self, other) -> bool:
+        pass
+
+    @abstractmethod
+    def __ge__(self, other) -> bool:
+        pass
+
+    @abstractmethod
+    def shift_empty_puzzle(self, direction: str) -> tuple:
+        pass
+
+    @abstractmethod
+    def set_metrics(self, heuristic: callable, g: int=None) -> None:
+        pass
+
+    class UnknownInstanceError(Exception):
+        def __init__(self, message='Unknown class instance', error=None):
+            super().__init__(message)
+            self.error = error
+
+    class BadMapError(Exception):
+        def __init__(self, message='Bad map', error=None):
+            super().__init__(message)
+            self.error = error
+
+
+class StatePQueueABC(PriorityQueue, metaclass=ABCMeta):
+
+    @abstractmethod
+    def __contains__(self, item: StateABC) -> bool:
+        pass
+
+    @abstractmethod
+    def __str__(self) -> str:
+        pass
+
+    def put_nowait(self, item):
+        if self.maxsize and self.maxsize == self.qsize():
+            self.pop_max()
+        return super().put_nowait(item)
+
+    def pop_max(self):
+        max_element = max(self.queue)
+        if not max_element:
+            return
+        max_index = self.queue.index(max_element)
+        return self.queue.pop(max_index)
+
+
+class StateDQueueABC(Deque, metaclass=ABCMeta):
+
+    @abstractmethod
+    def time_complexity(self) -> int:
+        pass
+
+    @abstractstaticmethod
+    def reverse_to_head(state: StateABC) -> iter:
+        pass
+
+    @abstractmethod
+    def __contains__(self, item: StateABC) -> bool:
+        pass
+
+    @abstractmethod
+    def __str__(self) -> str:
+        pass
---
+++ map_reader.py
@@ -0,0 +1,73 @@
+import os
+import re
+import numpy as np
+from utils import TERMINAL_STATES
+from state import State
+
+
+class NPuzzlesMap:
+
+    MIN_SHAPE = (3, 3)
+
+    def __init__(self, shape: tuple, initial_map: np.ndarray):
+        if shape < self.MIN_SHAPE:
+            raise self.BadMapError()
+        self.initial_map = initial_map
+        self.initial_state = State(self.initial_map)
+        dimension, _ = shape
+        terminal_array = TERMINAL_STATES.get(dimension)
+        self.terminal_state = State(terminal_array)
+
+    @staticmethod
+    def __map_from_file(filename: str) -> np.ndarray:
+        dimension = None
+        start_map = []
+        if not os.path.isfile(filename):
+            raise Exception(f'File {filename} does not exist')
+        with open(filename, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if dimension is None and re.fullmatch(r'\d+', line):
+                    dimension = int(line)
+                elif dimension is not None:
+                    row = re.findall(r'\d+', line)
+                    row = [int(digit) for digit in row[:dimension]]
+                    start_map.append(row)
+        return np.array(start_map)
+
+    @staticmethod
+    def __map_from_string(string_map: str) -> np.ndarray:
+        dimension = None
+        start_map = list()
+        lines = string_map.split('\n')
+        lines.pop(-1)
+        for line in lines:
+            line.strip()
+            if dimension is None and re.fullmatch(r'\d+', line):
+                dimension = int(line)
+            elif dimension is not None:
+                row = re.findall(r'\d+', line)
+                row = [int(digit) for digit in row[:dimension]]
+                start_map.append(row)
+        return np.array(start_map)
+
+    @classmethod
+    def from_file(cls, filename):
+        initial_map = cls.__map_from_file(filename)
+        return cls(initial_map.shape, initial_map)
+
+    @classmethod
+    def from_string(cls, string_map):
+        initial_map = cls.__map_from_string(string_map)
+        return cls(initial_map.shape, initial_map)
+
+    class BadMapError(Exception):
+        def __init__(self, message='Bad map', error=None):
+            super().__init__(message)
+            self.error = error
+
+    def __str__(self):
+        return f'Initial{self.initial_state}, Terminal{self.terminal_state}'
+
+    def __repr__(self):
+        return self.__str__()
